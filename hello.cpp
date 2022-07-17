d0944440e3d18d37bec1d48581a6424aa5cf4c37
trixsearach <inforatme@gmail.com>
2022-07-17 21:12:27 +0530

    new commit

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,8 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-    cout<<"Jai Shri Ram";
+    cout << "Jai Shri Ram";
     return 0;
+    system("pause");
 }
--- switchcase.cpp
+++ switchcase.cpp
@@ -1,21 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-int a;
-cout<<"type your answer: ";
-cin>>a;
-    switch (a) {
-        case 1:
-        cout<<"hello";
+    int a;
+    cout << "type your answer: ";
+    cin >> a;
+    switch (a)
+    {
+    case 1:
+        cout << "hello";
         break;
-        case 2:
-        cout<<"maa ki chuu";
+    case 2:
+        cout << "maa ki chuu";
         break;
-        case 3:
-        cout<<"tu hi dev";
+    case 3:
+        cout << "tu hi dev";
         break;
-        default:
-        cout<<"Tum sahi sey likho badda";
+    default:
+        cout << "Tum sahi sey likho badda";
     }
+    system("pause");
+    return 0;
 }
