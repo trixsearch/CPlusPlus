1dea3efb0bd39e4aae6218ea9d4b2eb2b738ab23
Abhinav sahu <163006809+iabhinavsahu@users.noreply.github.com>
2025-08-11 16:23:04 +0530

    Create SearchInrange.java
    linear search

---
+++ com/trix/SearchInrange.java
@@ -0,0 +1,23 @@
+package com.trix;
+
+public class SearchInRange {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println(searchRange(nums,1,4,3));
+    }
+
+    // using simple for loop
+    static int searchRange(int[]arr,int start,int end,int target){
+        int len = arr.length;
+        if(len==0){
+            return -1;
+        }
+        for(int i=start;i<=end;i++){
+            if(arr[i]==target){
+                return i;
+            }
+
+        }
+        return -1;
+    }
+}
