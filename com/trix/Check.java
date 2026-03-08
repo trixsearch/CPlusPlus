6d27479a587668c21a6d6e3d1b8df7927503ae58
Abhinav sahu <inforatme@gmail.com>
2026-03-09 01:48:40 +0530

    added some packages for developing basics

--- com/trix/Check.java
+++ com/trix/Check.java
@@ -1,5 +1,7 @@
+package com.trix;
+
 public class Check{
     public static void main(String[] args) {
-        System.out.println("HEllo");
+        System.out.println("HEfslo");
     }
 }
---
+++ com/trix/javainonevideo/datatypes/Test.java
@@ -0,0 +1,12 @@
+package com.trix.javainonevideo.datatypes;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("We are Learning Data types \n"+"=".repeat(30));
+        byte minByte=-128;
+        System.out.println("minimum value of a byte :"+minByte);
+        byte maxByte=127;
+        System.out.println("maximum value of a byte :"+maxByte);
+
+    }
+}
