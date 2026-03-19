4a091c3fc473372ec13d098f668a19e2a8092972
Abhinav1 Sahu <Abhinav1.Sahu@ril.com>
2026-03-19 13:13:35 +0530

    adding some files for java

---
+++ javaCode/StringsAndWork.java
@@ -0,0 +1,12 @@
+public class StringsAndWork {
+	public static void main(String []args) {
+		System.out.println("String Methods and Tutorials");
+		System.out.println("String Methods");
+		
+		String meriString = "ye Meri String h bhai dekhte Hai isme hum kya kar sakte hai ?";
+		
+		System.out.println("String real value : "+meriString);
+		System.out.println("String contains : "+meriString.contains("bhai"));
+		System.out.println("Length : "+ meriString.length());
+	}
+}
